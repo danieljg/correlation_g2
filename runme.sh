f3ffd8e7c4f599ccd3102a7bc0ffb5d3995de949
Daniel Jimenez <daniel.jimenez.gomez@gmail.com>
2014-06-02 16:58:30 -0700

    Added a runme to drive the simulator  On branch master  Changes to be committed: 	new file:   runme.sh

---
+++ runme.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo compiling
+make
+signal_apertures=(1 3 5)
+idler_apertures=(1 3 5)
+echo running
+for ((k=0;k<=2;k++))
+do
+ time ./coherence ${signal_apertures[$k]} ${idler_apertures[$k]} 21.0 21.0
+ gnuplot plot.p
+ cp coherence.eps graphs/signal_${signal_apertures[$k]}_idler_${idler_apertures[$k]}.eps
+done
+
