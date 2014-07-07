d8c1b8372038e497c21bbf19cf6d0684b756d61e
Daniel Jimenez <daniel.jimenez.gomez@gmail.com>
2014-07-06 21:16:38 -0700

    preparing for ssh run  On branch master  Your branch is up-to-date with 'origin/master'.  Changes to be committed: 	modified:   runme_angle.sh 	modified:   runme_temp.sh

--- runme_angle.sh
+++ runme_angle.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-
+#PBS -l nodes=1:ppn=1
+#PBS -l walltime=12:00:00
+#PBS -o outputfile
+#PBS -e errorfile
+#PBS -q default
+#PBS -V
+#PBS -N ERMjob_pcs
 echo compiling
 cd build/
 make coherence_angle
@@ -7,10 +13,9 @@ mv coherence_angle ../
 cd ..
 
 # apertures to try
-signal_ap=(0.3 1.0 3 5)
-idler_ap=0.3 #( 0.3 1.0 3 5)
+signal_ap=0.3 #(0.3 1.0 5)
+idler_ap=( 0.3 1.0 3.0 5)
 # configuration of detectors
-signal_angle=2.4
 idler_angle=2.4
 # working temp
 temp=51.5
@@ -22,8 +27,8 @@ rm data/angle/*.dat
 echo running
 for((k=0;k<5;k++))
 do
-time ./coherence_angle ${signal_ap[$k]} ${idler_ap} ${idler_angle} ${temp}
-cat coherence.dat >> data/angle/signal_${signal_ap[$k]}_idler_${idler_ap}.dat
+time ./coherence_angle ${signal_ap} ${idler_ap[$k]} ${idler_angle} ${temp}
+cat coherence.dat >> data/angle/signal_${signal_ap}_idler_${idler_ap[$k]}.dat
 done
 
 gnuplot plotting_scripts/plot_angle.p
--- runme_temp.sh
+++ runme_temp.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-
+#PBS -l nodes=1:ppn=1
+#PBS -l walltime=12:00:00
+#PBS -o outputfile
+#PBS -e errorfile
+#PBS -q default
+#PBS -V
+#PBS -N ERMjob_pcs
 echo compiling
 cd build/
 make coherence_temp
@@ -7,8 +13,8 @@ mv coherence_temp ../
 cd ..
 
 # apertures to try
-signal_ap=(0.3 1.0 5)
-idler_ap=0.3 #( 0.3 1.0 5)
+signal_ap=0.3 #(0.3 1.0 5)
+idler_ap=( 0.3 1.0 3.0 5)
 # configuration of detectors
 signal_angle=2.4
 idler_angle=2.4
@@ -20,8 +26,8 @@ rm data/temp/*.dat
 echo running
 for((k=0;k<5;k++))
 do
-time ./coherence_temp ${signal_ap[$k]} ${idler_ap} ${signal_angle} ${idler_angle}
-cat coherence.dat >> data/temp/signal_${signal_ap[$k]}_idler_${idler_ap}.dat
+time ./coherence_temp ${signal_ap} ${idler_ap[$k]} ${signal_angle} ${idler_angle}
+cat coherence.dat >> data/temp/signal_${signal_ap}_idler_${idler_ap[$k]}.dat
 done
 
 gnuplot plotting_scripts/plot_temp.p
